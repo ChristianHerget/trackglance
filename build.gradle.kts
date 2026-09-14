6e23ea7089ba63bf79a0d9fec2279900b49db0b5
Christian <christian@herget.email>
2026-09-14 12:50:43 +0200

    Update Kotlin, Compose, and Spotless dependencies (#73)
    * Update Kotlin and Compose compiler to 2.4.20
    
    * Update remaining Gradle dependencies
    
    * Use pinned CodeQL nightly for Kotlin 2.4.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "9.4.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
-    id("com.diffplug.spotless") version "8.10.1"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
+    id("com.diffplug.spotless") version "8.10.2"
     id("dev.detekt") version "2.0.0-alpha.6" apply false
     id("org.cyclonedx.bom") version "3.4.1" apply false
     id("org.spdx.sbom") version "0.12.0" apply false
